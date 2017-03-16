1f2e2d65a57f98aa9afa73ff4fac8f45fb7b22fd
Jeroen van Maanen <jeroen@sollunae.net>
2017-03-16 15:55:41 +0100

    Endpoint findProposals added.

--- src/main/java/com/thefuturegroup/zzpmatch/MatcherApplication.java
+++ src/main/java/com/thefuturegroup/zzpmatch/MatcherApplication.java
@@ -2,11 +2,23 @@ package com.thefuturegroup.zzpmatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class MatcherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatcherApplication.class, args);
 	}
-}
+
+    @RequestMapping(value = "findProposals", method = RequestMethod.GET)
+    public List<String> getProposals(String profileId) {
+        return new ArrayList<>();
+    }
+ }
