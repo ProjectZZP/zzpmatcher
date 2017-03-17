f4a1d33f97ca9d8b438dd5402e05de688d520484
Jeroen van Maanen <jeroen@sollunae.net>
2017-03-17 13:41:58 +0100

    Cleaned up superfluous imports.

--- src/main/java/com/thefuturegroup/zzpmatch/MatcherApplication.java
+++ src/main/java/com/thefuturegroup/zzpmatch/MatcherApplication.java
@@ -1,17 +1,13 @@
 package com.thefuturegroup.zzpmatch;
 
-import com.sun.javaws.exceptions.InvalidArgumentException;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
